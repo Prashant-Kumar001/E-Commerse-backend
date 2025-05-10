86bf3cf7bae3f45076197a12f7dcce47bf37779a
prashant-Kumar <prashant0kumar101@gmail.com>
2025-05-10 19:33:43 +0530

    refactor: streamline newUser function and improve code readability

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,77 +4,80 @@ import { asyncHandler } from "../middlewares/error.js";
 import { message, status } from "../utils/constants.js";
 
 const newUser = asyncHandler(async (req, res) => {
+  const { email, id, username, gender, dob, photo } = req.body;
 
+  console.log({
+    email,
+    id,
+    username,
+    gender,
+    dob,
+    photo,
+  })
 
-    const { email, id, username, gender, dob, photo } = req.body;
+  const userExists = await User.findById(id);
 
-    if (!id, !username || !email || !gender || !dob || !photo) {
-        throw new AppError("all fields are required", status.BAD_REQUEST);
+  if (userExists) {
+    return res.status(200).json({
+      success: true,
+      message: `welcome back ${userExists.username}`,
+    });
+  } else {
+    if ((!id, !username || !email || !gender || !dob || !photo)) {
+      throw new AppError("all fields are required", status.BAD_REQUEST);
     }
-
-    const userExists = await User.findOne(id);
-
-    if (userExists) {
-        return res.status(200).json({
-            success: true,
-            message: `welcome back ${userExists.username}`,
-        });
-    }
-
 
     const user = await User.create({
-        _id: id,
-        username,
-        email,
-        gender,
-        dob,
-        photo
+      _id: id,
+      username,
+      email,
+      gender,
+      dob,
+      photo,
     });
 
     res.status(201).json({
-        success: true,
-        message: message.USER_CREATED,
-        user
+      success: true,
+      message: message.USER_CREATED,
+      user: user.username,
     });
+  }
 });
 
-
 const allUsers = asyncHandler(async (req, res) => {
-    const users = await User.find();
-    res.status(200).json({
-        success: true,
-        users
-    });
+  const users = await User.find();
+  res.status(200).json({
+    success: true,
+    users,
+  });
 });
 
 const getUser = asyncHandler(async (req, res) => {
-    if (!req.params.id) {
-        throw new AppError("user not found", status.BAD_REQUEST);
-    }
-    const user = await User.findById(req.params.id);
-    if (!user) {
-        throw new AppError("user not found", status.NOT_FOUND);
-    }
-    res.status(200).json({
-        success: true,
-        user
-    })
-}
-);
+  if (!req.params.id) {
+    throw new AppError("user not found", status.BAD_REQUEST);
+  }
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    throw new AppError("user not found", status.NOT_FOUND);
+  }
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
 
 const deleteUser = asyncHandler(async (req, res) => {
-    if (!req.params.id) {
-        throw new AppError("enter user id", status.BAD_REQUEST);
-    }
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-        throw new AppError("invalid user id", status.NOT_FOUND);
-    }
-    res.status(200).json({
-        success: true,
-        message: 'user deleted successfully'
-    })
-}
-);
+  if (!req.params.id) {
+    throw new AppError("enter user id", status.BAD_REQUEST);
+  }
+  const user = await User.findByIdAndDelete(req.params.id);
+  if (!user) {
+    throw new AppError("invalid user id", status.NOT_FOUND);
+  }
+  res.status(200).json({
+    success: true,
+    message: "user deleted successfully",
+  });
+});
 
 export { newUser, allUsers, getUser, deleteUser };
